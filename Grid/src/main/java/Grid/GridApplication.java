0d94f6569a1b18a427ea8d5355357237a7c47476
Saksham <sakshamgupta5050@gmail.com>
2026-08-13 19:06:48 +0530

    Project initialization

---
+++ Grid/src/main/java/Grid/GridApplication.java
@@ -0,0 +1,13 @@
+package Grid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GridApplication.class, args);
+	}
+
+}
---
+++ Grid/src/test/java/Grid/GridApplicationTests.java
@@ -0,0 +1,13 @@
+package Grid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GridApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
